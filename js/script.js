c5e8630851fdb27e931747c4733d45375b572298
Albin <albin.nojback@gmail.com>
2021-03-19 10:00:47 +0100

    Add beginning of api requests

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Heya!");
+let API_URL = "http://www.omdbapi.com/?apikey=84f49e03";
+let IMG_API_URL = "http://img.omdbapi.com/?apikey=84f49e03";
+let output = document.querySelector("[data-js-output]");
+
+async function GetData() {
+  let response = await fetch(API_URL + "&t=pixels");
+
+  let data = await response.json();
+
+  console.log(data);
+  update(data);
+}
+
+function update(data) {
+  output.innerText = data.Title;
+}
